69ae1b5d5c4e8dfb5623dba42c6764acbc2c39ed
izaJepun <135489952+izaJepun@users.noreply.github.com>
2023-11-15 09:59:45 +0700

    app.py

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<h1>Hello, World!, Iza Ganteng</h1>"
+    return "<h1>Hello, World!, muhammad Dava Rahmadhana</h1>"
+     return "<h1>Hello, World!, Daffa Irfantoro</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
